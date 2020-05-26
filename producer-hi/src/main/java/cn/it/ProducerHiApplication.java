66398d17fd804b8acdf391a83e4c80a83b6afd9c
Liujunxi@gdcattsoft.com <2876564629@qq.com>
2020-05-26 16:07:41 +0800

    springcloud例子

--- producer-hi/src/main/java/cn/it/ProducerHiApplication.java
+++ producer-hi/src/main/java/cn/it/ProducerHiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- spring-config/src/main/java/cn/it/ConfigServerApplication.java
+++ spring-config/src/main/java/cn/it/ConfigServerApplication.java
@@ -2,13 +2,9 @@ package cn.it;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
-//@EnableEurekaClient
 @EnableConfigServer
 public class ConfigServerApplication {
     public static void main(String[] args) {
